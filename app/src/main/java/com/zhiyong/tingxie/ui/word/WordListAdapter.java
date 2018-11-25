ab80aa2a1703ea5f528364d3b0974668f4708279
Zhi Yong Tan <yongish@gmail.com>
2018-11-24 22:15:56 -0800

    Play sound. Open Baike dictionary.

--- app/src/main/java/com/zhiyong/tingxie/ui/word/WordListAdapter.java
+++ app/src/main/java/com/zhiyong/tingxie/ui/word/WordListAdapter.java
@@ -1,7 +1,11 @@
 package com.zhiyong.tingxie.ui.word;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,15 +18,27 @@ import com.zhiyong.tingxie.R;
 import com.zhiyong.tingxie.db.QuizPinyin;
 
 import java.util.List;
+import java.util.Locale;
 
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
     private final LayoutInflater mInflater;
+    private final TextToSpeech textToSpeech;
+    private final Context context;
 
     private List<WordItem> mWordItems;
 
-    WordListAdapter(Context context) {
+    WordListAdapter(final Context context) {
         mInflater = LayoutInflater.from(context);
+        textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.SIMPLIFIED_CHINESE);
+                }
+            }
+        });
+        this.context = context;
     }
 
     @NonNull
@@ -35,9 +51,25 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     @Override
     public void onBindViewHolder(@NonNull WordViewHolder holder, final int i) {
         if (mWordItems != null) {
-            WordItem current = mWordItems.get(i);
-            holder.tvWord.setText(current.getWord());
+            final WordItem current = mWordItems.get(i);
+            final String word = current.getWord();
+            holder.tvWord.setText(word);
             holder.tvPinyin.setText(current.getPinyin());
+            holder.ivPlay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    textToSpeech.speak(word, TextToSpeech.QUEUE_FLUSH, null);
+                }
+            });
+            holder.wordLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String url = "https://baike.baidu.com/item/" + word;
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    context.startActivity(intent);
+                }
+            });
         } else {
             holder.tvWord.setText("No Word");
             holder.tvPinyin.setText("No pinyin");
@@ -84,12 +116,14 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
         private final TextView tvWord;
         private final TextView tvPinyin;
         private final ImageView ivPlay;
+        private final ConstraintLayout wordLayout;
 
         private WordViewHolder(View itemView) {
             super(itemView);
             tvWord = itemView.findViewById(R.id.tvWord);
             tvPinyin = itemView.findViewById(R.id.tvPinyin);
             ivPlay = itemView.findViewById(R.id.ivPlay);
+            wordLayout = itemView.findViewById(R.id.wordLayout);
         }
     }
 }
