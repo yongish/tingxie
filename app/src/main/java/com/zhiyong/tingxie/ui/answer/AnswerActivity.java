2b4970bd7cdf98a2d6be2a285d151c8a4d9d218b
Zhi Yong Tan <yongish@gmail.com>
2018-12-01 22:51:19 -0800

    Generate AnswerActvity empty activity.

---
+++ app/src/main/java/com/zhiyong/tingxie/ui/answer/AnswerActivity.java
@@ -0,0 +1,15 @@
+package com.zhiyong.tingxie.ui.answer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhiyong.tingxie.R;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
+}
