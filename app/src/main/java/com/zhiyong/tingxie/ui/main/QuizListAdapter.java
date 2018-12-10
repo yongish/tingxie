a22a4fa4948ab268f84038857023a853747a4aca
Zhi Yong Tan <yongish@gmail.com>
2018-12-09 17:59:07 -0800

    show reset counter

--- app/src/main/java/com/zhiyong/tingxie/ui/main/QuizListAdapter.java
+++ app/src/main/java/com/zhiyong/tingxie/ui/main/QuizListAdapter.java
@@ -75,11 +75,10 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.QuizVi
 
             holder.tvDate.setText(displayDate);
             holder.tvWordsLeft.setText(String.format(Locale.US,
-                    "%d words",
-                    current.getTotalWords()));
-            // todo: Bug. Adding new word does not reset number of rounds to 0. Fix this in future.
-//                    "%d/%d remaining on round %d",
-//                    current.getNotLearned(), current.getTotalWords(), current.getRound()));
+//                    "%d words",
+//                    current.getTotalWords()));
+                    "%d/%d remaining on round %d",
+                    current.getNotLearned(), current.getTotalWords(), current.getRound()));
 
             holder.ivEditDate.setOnClickListener(new View.OnClickListener() {
                 @Override
