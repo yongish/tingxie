85f89e81bbea2d88f108f7e1a679836c1a27afeb
Zhi Yong Tan <yongish@gmail.com>
2019-10-31 22:34:24 -0700

    started Term.java, but may skip it in favor of directly calling API.

---
+++ app/src/main/java/com/zhiyong/tingxie/db/Term.java
@@ -0,0 +1,43 @@
+package com.zhiyong.tingxie.db;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity()
+public class Term {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private long id;
+
+    private long createdAt;
+
+    private long deletedAt;
+
+    private long quizId;
+
+    private String userId;
+
+    private String word;
+
+    private String pinyin;
+
+    private int timesCorrect;
+
+    public Term(@NonNull long createdAt, @NonNull long deletedAt, @NonNull long quizId,
+                @NonNull String userId, @NonNull String word, @NonNull String pinyin) {
+        this.createdAt = createdAt;
+        this.deletedAt = deletedAt;
+        this.quizId = quizId;
+        this.userId = userId;
+        this.word = word;
+        this.pinyin = pinyin;
+    }
+
+    public void setId(@NonNull long id) {
+        this.id = id;
+    }
+
+    public void setTimesCorrect(@NonNull long quizId, @NonNull String userId, @NonNull String word,
+                                )
+}
