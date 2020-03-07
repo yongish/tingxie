589c6243d4262b1729e7e00d86065926700bed87
Zhi Yong Tan <yongish@gmail.com>
2020-03-06 20:09:36 -0800

    get token

--- app/src/main/java/com/zhiyong/tingxie/ui/login/LoginActivity.java
+++ app/src/main/java/com/zhiyong/tingxie/ui/login/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.zhiyong.tingxie.ui.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.zhiyong.tingxie.ui.main.MainActivity;
 
 import java.util.Arrays;
@@ -63,6 +71,22 @@ public class LoginActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+                        if (task.isSuccessful()) {
+                            String idToken = task.getResult().getToken();
+
+                            SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("login", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString("token", idToken);
+                            editor.apply();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error in login. Contact the developer.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
                 // ...
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
