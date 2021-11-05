60dff8c054bba0fc66f24239f6573b1ad1546671
Zhi Yong Tan <yongish@gmail.com>
2021-11-05 15:35:26 -0700

    Replace QuizItem.java with QuizItem.kt.

---
+++ app/src/main/java/com/zhiyong/tingxie/ui/main/QuizItem.kt
@@ -0,0 +1,12 @@
+package com.zhiyong.tingxie.ui.main
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class QuizItem(val id: Long,
+                    var date: Int,
+                    var title: String,
+                    var totalWords: Int,
+                    var notLearned: Int,
+                    var round: Int) : Parcelable
