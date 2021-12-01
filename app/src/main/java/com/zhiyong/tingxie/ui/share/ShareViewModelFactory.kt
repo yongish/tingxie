4e21b981bf88f848d1d3e3b4c922521f29829001
Zhi Yong Tan <yongish@gmail.com>
2021-11-30 22:17:44 -0800

    fix

--- app/src/main/java/com/zhiyong/tingxie/ui/share/ShareViewModelFactory.kt
+++ app/src/main/java/com/zhiyong/tingxie/ui/share/ShareViewModelFactory.kt
@@ -7,11 +7,10 @@ import androidx.lifecycle.ViewModelProvider
 class ShareViewModelFactory(
     private val quizId: Long,private val application: Application
     ) : ViewModelProvider.Factory {
-  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
     if (modelClass.isAssignableFrom(ShareViewModel::class.java)) {
       return ShareViewModel(quizId, application) as T
     }
     throw IllegalArgumentException("Unknown ViewModel class")
   }
-
 }
