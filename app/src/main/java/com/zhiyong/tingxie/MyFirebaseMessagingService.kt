e7b864cda0cf3dfcee78d7eef9ef405536244119
Zhi Yong Tan <yongish@gmail.com>
2023-01-14 23:48:31 -0800

    Current implementing handling data. Next step is to complete for all ~9 actions.

--- app/src/main/java/com/zhiyong/tingxie/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/zhiyong/tingxie/MyFirebaseMessagingService.kt
@@ -5,17 +5,17 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
-import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.zhiyong.tingxie.network.NetworkQuiz
 import com.zhiyong.tingxie.network.NetworkToken
 import com.zhiyong.tingxie.network.TingXieNetwork
 import com.zhiyong.tingxie.ui.main.MainActivity
+import com.zhiyong.tingxie.ui.question.RemoteQuestionActivity
+import com.zhiyong.tingxie.ui.word.WordActivity
 import kotlinx.coroutines.runBlocking
 
 
@@ -23,35 +23,38 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
   override fun onMessageReceived(p0: RemoteMessage) {
     super.onMessageReceived(p0)
-
-    // key-value pairs, in case we want to use them.
-//    p0.data
-
-//    p0.notification
-
-//    Handler(Looper.getMainLooper()).post {
-//      Toast.makeText(applicationContext, "Quiz shared with you.", Toast.LENGTH_SHORT).show()
-//    }
-
     sendNotification(p0)
-//    with (p0.notification?.title?.uppercase() ?: "") {
-//      when {
-//        contains("QUIZ SHARED") -> sendNotification(p0.notification)
-//        else -> ""
-//      }
-//
-//    }
   }
 
-//  private fun sendNotification(messageBody: String) {
   private fun sendNotification(p0: RemoteMessage) {
-    val intent = Intent(this, MainActivity::class.java)
+    val activity = when (p0.data["action"]) {
+      "" -> MainActivity::class.java
+      else -> MainActivity::class.java
+    }
+//    val intent = Intent(this, activity)
+
+    val intent = Intent(this, WordActivity::class.java)
+    intent.putExtra(
+      RemoteQuestionActivity.EXTRA_QUIZ_ITEM,
+      NetworkQuiz(
+        p0.data["entityId"]?.toLong() ?: -1,
+        p0.data["title"] ?: "NO TITLE",
+        p0.data["date"]?.toInt() ?: -1,
+        p0.data["email"] ?: "NO EMAIL",
+        p0.data["role"] ?: "NO ROLE",
+        p0.data["numWords"]?.toInt() ?: -1,
+        p0.data["numNotCorrect"]?.toInt() ?: -1,
+        p0.data["round"]?.toInt() ?: -1
+      )
+    )
+
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-    val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-      PendingIntent.FLAG_IMMUTABLE)
+    val pendingIntent =
+      PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
     val channelId = "fcm_default_channel"
-    val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+    val defaultSoundUri =
+      RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
     val notificationBuilder = NotificationCompat.Builder(this, channelId)
       .setContentTitle(p0.notification?.title)
       .setSmallIcon(R.drawable.icon)
@@ -61,7 +64,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
       .setSound(defaultSoundUri)
       .setContentIntent(pendingIntent)
 
-    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val notificationManager =
+      getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
   }
 
