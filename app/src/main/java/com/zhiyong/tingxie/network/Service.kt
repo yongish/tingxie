b21d245d687f57ec43338b700953c79355215ec6
Zhi Yong Tan <yongish@gmail.com>
2023-01-29 17:41:27 -0800

    versionCode 52

--- app/src/main/java/com/zhiyong/tingxie/network/Service.kt
+++ app/src/main/java/com/zhiyong/tingxie/network/Service.kt
@@ -190,8 +190,8 @@ interface Service {
 
 object TingXieNetwork {
   private val retrofit = Retrofit.Builder()
-    .baseUrl("http://10.0.2.2:9000/") // Emulator
-//    .baseUrl("https://h1b1club.com/") // Production URL.
+//    .baseUrl("http://10.0.2.2:9000/") // Emulator
+    .baseUrl("https://h1b1club.com/") // Production URL.
     .addConverterFactory(MoshiConverterFactory.create().asLenient())
     .build()
 
