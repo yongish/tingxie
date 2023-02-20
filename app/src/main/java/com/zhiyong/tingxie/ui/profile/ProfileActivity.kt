14e84729797822027416f76544900e2bfcaf777e
Zhi Yong Tan <yongish@gmail.com>
2023-02-20 14:01:32 -0800

    ProfileActivity uses DrawerActivity.

--- app/src/main/java/com/zhiyong/tingxie/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/zhiyong/tingxie/ui/profile/ProfileActivity.kt
@@ -1,14 +1,12 @@
 package com.zhiyong.tingxie.ui.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zhiyong.tingxie.R
+import com.zhiyong.tingxie.ui.DrawerActivity
 
-class ProfileActivity : AppCompatActivity() {
-
+class ProfileActivity : DrawerActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_profile)
     if (savedInstanceState == null) {
       supportFragmentManager.beginTransaction()
         .replace(R.id.container, ProfileFragment.newInstance())
