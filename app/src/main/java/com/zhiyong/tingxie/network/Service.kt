7e89ab6f71cddbdde51c0171b0bf8e37d8a4d104
Zhi Yong Tan <yongish@gmail.com>
2023-04-21 23:27:09 -0700

    Followed https://mobikul.com/android-retrofit-handling-sslhandshakeexception/ to try to fix certificate error. versionCode 58

--- app/src/main/java/com/zhiyong/tingxie/network/Service.kt
+++ app/src/main/java/com/zhiyong/tingxie/network/Service.kt
@@ -1,10 +1,16 @@
 package com.zhiyong.tingxie.network
 
 import com.zhiyong.tingxie.ui.main.MigrateLocal
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.*
+import javax.net.ssl.*
+
 
 interface Service {
   @PUT("tokens")
@@ -189,10 +195,44 @@ interface Service {
 }
 
 object TingXieNetwork {
+  private fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+    return try {
+      // Create a trust manager that does not validate certificate chains
+      val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+        object : X509TrustManager {
+          @Throws(CertificateException::class)
+          override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String?) {
+          }
+
+          @Throws(CertificateException::class)
+          override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String?) {
+          }
+
+          override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+          }
+        }
+      )
+
+      // Install the all-trusting trust manager
+      val sslContext: SSLContext = SSLContext.getInstance("SSL")
+      sslContext.init(null, trustAllCerts, SecureRandom())
+
+      // Create an ssl socket factory with our all-trusting manager
+      val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+      val builder = OkHttpClient.Builder()
+      builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+      builder.hostnameVerifier { _, _ -> true }
+      builder
+    } catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+  }
   private val retrofit = Retrofit.Builder()
 //    .baseUrl("http://10.0.2.2:9000/") // Emulator
     .baseUrl("https://h1b1club.com/") // Production URL.
     .addConverterFactory(MoshiConverterFactory.create().asLenient())
+    .client(getUnsafeOkHttpClient().build())
     .build()
 
   val tingxie: Service = retrofit.create(Service::class.java)
